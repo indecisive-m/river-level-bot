85bcd517993f79f2a82a6dd13dea94dee5901824
indecisive-m <michr.watkins@btinternet.com>
2024-12-28 18:02:45 +0000

    Added some functionality to the bot

---
+++ events/sendMessage.js
@@ -0,0 +1,11 @@
+const { Events } = require("discord.js");
+
+module.exports = {
+  name: Events.ClientReady,
+  once: true,
+  execute(client) {
+    const channel = client.channels
+      .fetch("1321994435185213483")
+      .then((channel) => channel.send("hello"));
+  },
+};
