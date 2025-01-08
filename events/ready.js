8f12c3a9bc73216cb80def500f6eb0fbdb9e33ef
indecisive-m <michr.watkins@btinternet.com>
2025-01-08 21:58:40 +0000

    Made a cloudflare worker but I think it will be too slow to run on the free workers plan.

--- events/ready.js
+++
@@ -1,9 +0,0 @@
-import { Events } from "discord.js";
-
-export const name = Events.ClientReady;
-
-export const once = true;
-
-export function execute(client) {
-  console.log(`Ready! Logged in as ${client.user.tag}`);
-}
